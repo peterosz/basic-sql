2a6efad86452e888dd3914ccb19808dba270e1c9
peterosz <itharius@gmail.com>
2017-05-09 20:15:47 +0200

    question 6 done

--- main.py
+++ main.py
@@ -109,7 +109,11 @@ def new_applicant():
 
 
 def jeminas_new_number():
-    pass
+    cur.execute("""UPDATE applicants SET phone_number='003670/223-7459'
+                WHERE first_name='Jemima' AND last_name='Foreman';""")
+    cur.execute("""SELECT first_name, last_name, phone_number FROM applicants
+                WHERE first_name='Jemima' AND last_name='Foreman';""")
+    print_table(table=cur.fetchall())
 
 
 def cancel_application():
