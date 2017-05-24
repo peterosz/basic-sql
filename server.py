3c3d99c69c2bec3f3a377b6d2f4e03b3f1427df0
peterosz <itharius@gmail.com>
2017-05-24 17:20:31 +0200

    routing done in server.py

--- server.py
+++ server.py
@@ -4,3 +4,32 @@ from flask import Flask, render_template
 @app.route('/')
 def index_page():
     return render_template('index.html')
+
+
+@app.route('/all-school')
+def show_all_school():
+    return data_manager.all_school()
+
+
+@app.route('/mentors-by-country')
+def show_mentors_by_country():
+    return data_manager.mentors_by_country()
+
+
+@app.route('/contacts')
+def show_contacts():
+    return data_manager.contacts()
+
+
+@app.route('/applicants')
+def show_applicants():
+    return data_manager.applicants()
+
+
+@app.route('/applicants-and-mentors')
+def show_applicants_and_mentors():
+    return data_manager.applicants_and_mentors()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
